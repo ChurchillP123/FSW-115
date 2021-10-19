d2f2bdd299f48d6b2abdebe62b21325d5a6512d8
Churchill perry <churchill.perry@bryanuniversity.edu>
2021-10-18 20:51:26 -0500

    created a getData function

--- week3/pokemonList/pokemon.js
+++ week3/pokemonList/pokemon.js
@@ -1,59 +1,35 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://pokeapi.co/api/v2/', true);
-xhr.send();
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        let data = JSON.parse(xhr.responseText);
-        getPokemonNames(data);
-    } else if (xhr.readyState === 4 && xhr.status !== 200) {
-        console.log(xhr.responseText);
-    }
+function getData(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.send();
+    xhr.onload = () => (xhr.status === 200) ? callback(xhr.response) : console.log(xhr.response);
+    xhr.onerror = () => console.error(xhr.response);
+}
+
+getData('https://pokeapi.co/api/v2/', toArray);
+
+function toArray(data) {
+    let arr = Object.entries(data);
+    let pokemon = arr[36][1];
+    getData(pokemon, getPokemonNames);
 }
 
 function getPokemonNames(data) {
-    let pokemonNameEndpoint = data.pokemon;
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', pokemonNameEndpoint, true);
-    xhr.send();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let data = JSON.parse(xhr.responseText);
-            postToDOM(data);
-        } else if (xhr.readyState === 4 && xhr.status !== 200) {
-            console.log(xhr.responseText);
-        }
-    }   
+    let results = data.results;
+    postToDOM(results);
 }
 
 function postToDOM(data) {
-    for (let i = 0; i < data.results.length; i++) {
-        let div = document.createElement('div');
-        let pokeName = document.createElement('h1');
-        pokeName.textContent = `Name: ${data.results[i].name}`;
-        pokeName.style.textAlign = 'center';
-        let pokeHeight = document.createElement('h2');
-        getPokemonInfo(data.results[i].url, pokeHeight);
-        pokeHeight.style.textAlign = 'center';
-        div.append(pokeName, pokeHeight);
-        div.style.border = '2px solid black';
-        div.style.margin = '10px';
-        document.body.appendChild(div);
+    for (let i = 0; i < data.length; i++) {
+        let name = document.createElement('h1');
+        name.textContent = data[i].name;
+        document.body.appendChild(name);
+        getData(data[i].url, getHeight);
     }
 }
 
-
-
-function getPokemonInfo(url, pokeHeight) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.send();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            let data = JSON.parse(xhr.responseText);
-            pokeHeight.textContent = `Height: ${data.height}`;
-        } else if (xhr.readyState === 4 && xhr.status !== 200) {
-            console.log(xhr.responseText);
-        }
-    } 
-    
+function getHeight(data) {
+    //I'm not sure how to access the heights and tag them back to the postToDOM function
+    console.log(data);
 }
